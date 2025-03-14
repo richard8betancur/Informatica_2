f804dcdb27fae3394ac4207ec3b45206bbaa9165
Richard Betancur <richard.vbetancur@udea.edu.co>
2025-03-14 10:29:25 -0500

    listoEjercicio2

--- lab_1/Ejercicios/main.cpp
+++ lab_1/Ejercicios/main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+void Ejercicio1(){
+
+    int numero;
+    cout << "Por favor ingrese un numero: ";
+    cin >> numero;
+
+    if (numero % 2 == 0){
+        cout << "El numero " << numero << " es par. " << endl;
+    } else {
+        cout << "El numero " << numero << " es impar. " << endl;
+    }
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    Ejercicio1();
+
     return 0;
 }
