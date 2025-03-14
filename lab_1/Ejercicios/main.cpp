be809b45c6c177c83a2a2031cd0f25e31156f972
Richard Betancur <richard.vbetancur@udea.edu.co>
2025-03-14 10:53:04 -0500

    listoEjercicio4

--- lab_1/Ejercicios/main.cpp
+++ lab_1/Ejercicios/main.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
-void Ejercicio1(){
+/*
+ * 2. Escriba un problema que pida un numero N e imprima en pantalla si es par o impar.
+ */
+void Ejercicio2();
+
+/*
+ * 4. Escriba un problema que pida dos numeros A y B e imprima en pantalla el menor.
+ */
+int Ejercicio4();
+
+
+//1. Ejercicio #2:
+void Ejercicio2(){
 
     int numero;
     cout << "Por favor ingrese un numero: ";
@@ -15,9 +27,25 @@ void Ejercicio1(){
     }
 }
 
+int Ejercicio4(){
+    int a, b;
+    cout << "Ingrese un numero: ";
+    cin >> a;
+    cout << "Ingrese otro numero: ";
+    cin >> b;
+
+    if (a < b){
+        cout << "El numero " << a << " es menor que el numero " << b << endl;
+    } else if (b < a){
+        cout << "El numero " << b << " es menor que el numero " << a << endl;
+    } else {
+        cout << "Los numeros " << a << " y " << b << " son iguales. " << endl;
+    }
+    return 0;
+}
+
 int main()
 {
-    Ejercicio1();
-
+    Ejercicio4();
     return 0;
 }
