586451129426fb6a3aa2eb21ec2a02459556dc9b
Sebastián Molina <s.molina1@udea.edu.co>
2025-03-26 15:52:16 -0500

    Avance Problema8

--- lab_1/Problemas/main.cpp
+++ lab_1/Problemas/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -198,9 +199,45 @@ void Problema6(){
 //4. Problema #8:
 void Problema8(){
 
-    int a;
-    cin >> a;
-    cout << a << endl;
+
+    int a, b, c;
+    while(true){
+        cout << "Ingrese dos numeros a y b para sumar: ";
+        cin >> a;
+        cin >> b;
+        cout << "Ingrese un numero c para el limite de la suma: ";
+        cin >> c;
+
+        if (cin.fail()){
+            cout << "Error, ingresa un valor invalido. " << endl;
+
+            cin.clear();
+            cin.ignore(1000, '\n');
+            continue;
+
+        }
+        break;
+    }
+    vector<int> multiplosA;
+    vector<int> multiplosB;
+    int resultado1 = 0;
+    int resultado2 = 0;
+
+    for(int i = 1; a * i <= c; i++){
+        multiplosA.push_back(a*i);
+        resultado1 = (a*i);
+        cout << resultado1;
+    }
+
+    for(int i = 1; b * i <= c; i++){
+        multiplosB.push_back(b*i);
+        resultado2 = (b*i);
+        cout << resultado2;
+    }
+
+    if(resultado1[] != resultado2[]){
+        cout << resultado1 << "+" << resultado2;
+    }
 
 }
 
